0e60faa9ea73a49cf6e062eb4b3b330ca8cf9d42
infonic-LiTianqi <129127346+infonic-LiTianqi@users.noreply.github.com>
2024-07-30 12:42:24 +0900

    選択リスト検索実装

---
+++ force-app/main/default/lwc/inventoryControl/inventoryControl.js
@@ -0,0 +1,40 @@
+import { LightningElement } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
+import getItemTypeList from '@salesforce/apex/InventoryControlController.getItemTypeList';
+
+export default class InventoryControl extends NavigationMixin(LightningElement) {
+
+  //品目種別の値
+  selectedType;
+  //品目種別の選択肢
+  options;
+
+
+  /*
+  The connectedCallback() lifecycle hook fires when a component is inserted into the DOM. 
+  The disconnectedCallback() lifecycle hook fires when a component is removed from the DOM
+  */
+  async connectedCallback(){
+    this.options = await this.getoptions();
+    console.log(this.options);
+  }
+
+
+  async getoptions(){
+    try{
+      const itemTypeList = await getItemTypeList();
+      return itemTypeList;
+    } catch(e){
+      console.log(e);
+    }
+  }
+
+  handleChange(event){
+    try{
+      this.selectedType = event.target.value;
+      console.log(this.selectedType);
+    } catch(e){
+      console.log(e);
+    }
+  }
+}
