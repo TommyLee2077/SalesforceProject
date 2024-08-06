18265ca01215b3092202243a985b28376ccf273b
infonic-LiTianqi <129127346+infonic-LiTianqi@users.noreply.github.com>
2024-08-06 17:09:12 +0900

    検索処理を一旦push

--- force-app/main/default/lwc/inventoryControl/inventoryControl.js
+++ force-app/main/default/lwc/inventoryControl/inventoryControl.js
@@ -2,12 +2,22 @@ import { LightningElement } from 'lwc';
 import { ShowToastEvent } from "lightning/platformShowToastEvent";
 
 import getItemTypeList from '@salesforce/apex/InventoryControlController.getItemTypeList';
+import getICList from '@salesforce/apex/InventoryControlController.getICList';
 
+const columns = [
+  {label:'品目種別',fieldName:'ItemType__c'},
+  {label:'品目名',fieldName:'ItemName__c'},
+  {label:'仕入先名',fieldName:'Supplier__c'},
+  {label:'価格',fieldName:'Price__c'},
+  {label:'在庫数',fieldName:'StockQuantity__c',type:'number',editable:true},
+  {label:'原価',fieldName:'PrimeCost__c'},
+  {label:'原価合計額',fieldName:'CostTotalAmount__c'}
+];
 
 export default class InventoryControl extends LightningElement {
 
   //品目種別の値
-  selectedType;
+  selectedType = 'allType';
   //品目種別の選択肢
   options;
 
@@ -15,13 +25,20 @@ export default class InventoryControl extends LightningElement {
   searchName;
   
   //チェックボックス初期値
-  checkedValue = ['isFalse'];
+  checkedValue = ['False'];
+  isExist = false;
 
   searchDisabled = false;
 
+  //dataTable表示制御
+  displayDatatable = false;
+
+  columns = columns;
+  ICData = [];
+
   get checkBoxOptions(){
     return [
-      {label:'在庫あり',value:'isTrue'}
+      {label:'在庫あり',value:'True'}
     ]
   }
 
@@ -31,6 +48,7 @@ export default class InventoryControl extends LightningElement {
   */
   async connectedCallback(){
     this.options = await this.getoptions();
+    
     console.log(this.options);
     console.log(this.checkedValue[0])
   }
@@ -66,14 +84,19 @@ export default class InventoryControl extends LightningElement {
   handleCheckChange(event){
     try{
       this.checkedValue = event.detail.value;
-      console.log(this.checkedValue[0]);
+      this.isExist = this.checkedValue[0]?true:false;
+      console.log(this.isExist);
     }catch(e){
       console.log(e);
     }
   }
 
-  search(){
-
+  async search(){
+    
+    console.log(this.selectedType,this.searchName,this.isExist)
+    this.ICData = await getICList({ItemType:this.selectedType,ItemName:this.searchName,isExist:this.isExist});
+    console.log(this.ICData);
+    this.displayDatatable = true;
   }
 
 
